533e492d1c15caf3155029d818267e150b959c87
wannaBpark <leehun456@hanmail.net>
2025-03-21 20:40:27 +0900

    - 메시 컴포넌트, 메시 데이터 관련 구조체 및 클래스 생성 [Object/Mesh] - ObjInfo.h / StaticMesh.h / UStaticMesh.h [Object/StaticMeshComponent] - MeshComponent UStaticMeshComp / UMeshComp TODO - 각 클래스들의 멤버함수, 렌더 및 상수버퍼 업데이트 함수 작성 - FStaticMesh에 저장될 자료 정보를 결정하고, ObjInfo로부터 무엇을 받아 저장할지 결정 필요

--- Source/Object/StaticMeshComponent/MeshComponent.h
+++ Source/Object/StaticMeshComponent/MeshComponent.h
@@ -1,21 +1,58 @@
 #pragma once
 #include "Object/PrimitiveComponent/UPrimitiveComponent.h"
+#include "Object/Mesh/UStaticMesh.h"
+#include "SimpleJSON/Json.hpp"
 
+/*
+* 3D 메시를 렌더링하고 관리하는 컴포넌트
+* 머티리얼, 충돌, 트랜스폼을 관리
+*/
+class JSON;
 class UMeshComponent : public UPrimitiveComponent
 {
 	using Super = UPrimitiveComponent;
 public:
-	UMeshComponent() = default;
+    UMeshComponent() = default;
 
 public:
 	virtual void BeginPlay() override;
 	virtual void Tick(float DeltaTime) override;
-	virtual void Render() override;
+	virtual void Render() override;								// [TODO] Render시 Preload된 UStaticMesh 인스턴스에 대한 GPU 메모리[Vertex Buffer, IL] 생성
 
 	void SetMeshName(FName InMeshName) { MeshName = InMeshName; }
 	FName GetMeshName() const { return MeshName; }
 
+	virtual void Serialize(bool bIsLoading, JSON Handle) = 0;
+
 private:
 	FName MeshName;
 
+};
+
+/*
+* 특정 Actor에 부착되어 메시 렌더링을 담당하는 컴포넌트
+* @UStaticMesh : 에셋 참조 - FObjManager를 통해 에셋 로드
+*/
+class UStaticMeshComp : public UMeshComponent
+{
+    UStaticMesh* StaticMesh;
+
+    using Super = UMeshComponent;
+    void Serialize(bool bIsLoading, JSON Handle)
+    {
+        Super::Serialize(bIsLoading, Handle);
+
+        if (bIsLoading)
+        {
+            FString assetName;
+            Handle << "ObjStaticMeshAsset" << assetName;
+            //StaticMesh = FObjManager::LoadObjStaticMesh(assetName);
+        }
+        else
+        {
+            FString assetName;
+            //FString assetName = StaticMesh->GetAssetPathFileName();
+            Handle << "ObjStaticMeshAsset" << assetName;
+        }
+    }
 };
